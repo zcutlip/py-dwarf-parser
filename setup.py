48bfe45b5ffce64f061d2950fd5b04a773e16d8c
Zachary Cutlip <uid000@gmail.com>
2019-07-01 12:00:42 -0700

    setup.py installer

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pydwarf',
+      version='0.1',
+      description='A package to parse DWARF-formatted debugging information.',
+      url="TBD",
+      packages=find_packages(),
+      python_requires='>=2.7',
+      install_requires=[]
+      )
