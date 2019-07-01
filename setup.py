db05183fb66dea890f907690c2caf16d7893dc39
Zachary Cutlip <uid000@gmail.com>
2019-07-01 15:07:49 -0700

    update name of project in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='pydwarf',
+setup(name='py-dwarf-parser',
       version='0.1',
       description='A package to parse DWARF-formatted debugging information.',
       url="TBD",
