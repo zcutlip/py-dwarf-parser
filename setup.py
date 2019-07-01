e8a9d88d70d22ed6d548a0fd5d355c4013a35411
Zachary Cutlip <uid000@gmail.com>
2019-07-01 15:23:14 -0700

    set url to github in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='py-dwarf-parser',
       version='0.1',
       description='A package to parse DWARF-formatted debugging information.',
-      url="TBD",
+      url="https://github.com/zcutlip/py-dwarf-parser",
       packages=find_packages(),
       python_requires='>=2.7',
       install_requires=[]
