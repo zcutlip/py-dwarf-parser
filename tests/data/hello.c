760df1ae77fcd69d2b387de021679426b8e84c14
Zachary Cutlip <uid000@gmail.com>
2019-10-16 15:15:04 -0700

    add test input data

---
+++ tests/data/hello.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int main(int argc, char **argv)
+{
+    int ret=0;
+
+    printf("Hello world\n");
+    //insert code here
+
+    return ret;
+}
+
